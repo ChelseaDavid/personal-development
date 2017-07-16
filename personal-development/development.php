4f7269dac6b1065fed8428949aabb0ffd728f88f
Chelsea David <cryan17@cnm.edu>
2017-07-16 10:51:36 -0600

    created prof dev plan

---
+++ personal-development/development.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Personal Development</title>
+	</head>
+	<body>
+		<h1>Assignment:</h1>
+		<!-- Quantitative -->
+		<h2>3 Quantitative Qualities:</h2>
+		<ul>
+			<li>Responsible for lowest Physical Inventory right off in company history 2016 with a total
+				right off in inventory of $0.97.</li>
+			<li>Closed 778 homes year 2016</li>
+			<li>Successfully navigated the local branch to "Branch of the Quarter" with record breaking
+				month end sales reports (18% above plan).</li>
+		</ul>
+		<!-- Qualitative -->
+		<h2>3 Qualitative Qualities:</h2>
+		<ul>
+			<li>Punctual: always first in last out. Installers tend to get up and started between
+				6 AM and 7</li>
+			<li>Non-Profit work. Affiliations - Wounded Warrior, Habbitat for Humanity, Boys and Girls club ranch
+				, Los Ranchos Boys Placement Housing</li>
+			<li>Professional: Somewhat of a chameleon(in the best sense of course), I can adapt
+				to the people and places of my surroundings. Which, being a female in a primarily
+				"Mans driven" field, you almost have to be able to adapt.</li>
+			<li>Clicker: most times you can find the answer yourself if you just "click"</li>
+		</ul>
+			<!-- What, How, Why -->
+		<p	><strong>What -</strong>Sub/Independent Contractors & tradesman</p>
+		<p><strong>How -</strong> creating a platform for these installers to advertise work available
+			while simultaneously building a profile / online resume for future employers to view.
+			(The age old saying " no one is going to call you for a job" is done ;-))</p>
+		<p><strong>Why -</strong> Most of these tradesman ( Excluding Electrical) do not require
+			licenses to install. Since doing away with it, they do not have a reliable profile / rating system
+			to set them apart from anyone else. ( Person takes a tile class at Home Depot, thinks they are a
+			tile installer)</p>
+		<!-- Elevator Pitch -->
+		<p><strong>Elevator Pitch-</strong></p>
+		<p>Hi, My name is Chelsea. I work with tradesman, sub & independent contractors, creating a platform
+			they may use to gain employment during slow times, and simultaneously build professional profiles
+			and resumes. I am using my coding skills to make this dream a reality.<p>
+		<p>Questions:</p>
+		<p>Do you need work done on your home or business?</p>
+		<p>Do you need an installer or know of one looking for work?</p>
+		<p>Can't decide which question to use. Any and all critique is appreciated!
+			Hope you are enjoying your weekend!</p>
+	</body>
+</html>
